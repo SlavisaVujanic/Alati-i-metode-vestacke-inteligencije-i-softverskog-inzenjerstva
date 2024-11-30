c5627ab7d9ddaaeb61d36af1bc35ee4936473c26
SlavisaVujanic <svujaniic@gmail.com>
2024-11-30 21:47:05 +0100

    Labels and button

--- src/project/gui.clj
+++ src/project/gui.clj
@@ -9,14 +9,21 @@
       par3-label (new JLabel "Par3")
       button (new JButton "Btn")]
 
+  (.setLayout frame nil)
+
+  (.setBounds par1-label 0 0 50 50)
+  (.setBounds par2-label 50 0 50 50)
+  (.setBounds par3-label 100 0 50 50)
+  (.setBounds button 200 100 100 50)
+
 
   (doto frame
-    (.setLayout (new GridLayout 3 1))
     (.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE)
     (.setSize 640 360)
     (.setResizable false)
     (.setLocationRelativeTo nil)
     (.setVisible true)
-    (. add par1-label)
-    (. add par2-label)
-    (. add par3-label)))
+    (.add par1-label)
+    (.add par2-label)
+    (.add par3-label)
+    (.add button)))
