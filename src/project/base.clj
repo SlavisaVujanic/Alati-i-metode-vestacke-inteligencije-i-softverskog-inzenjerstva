68f82657144b8bbf24da079d7ab4501192b93272
SlavisaVujanic <svujaniic@gmail.com>
2024-12-22 11:59:03 +0100

    Add and delete user

--- src/project/base.clj
+++ src/project/base.clj
@@ -1,18 +1,29 @@
 (ns project.base
-  (:require [clojure.java.jdbc :as jdbc]))
+  (:require [clojure.java.jdbc :as jdbc]
+            [clojure.string :as str]))
 
-(defn create-db-spec []
+(defn create-db-spec [host dbname]
   {:dbtype "mssql"
-   :host (System/getenv "DB_HOST")
-   :port (System/getenv "DB_PORT")
-   :dbname (System/getenv "DB_NAME")
+   :host host
+   :port 1433
+   :dbname dbname
    :properties {:integratedSecurity "true"}})
 
-(defn authenticate [username password]
-  (let [db-spec (create-db-spec)
+(defn authenticate [host dbname username password]
+  (let [db-spec (create-db-spec host dbname)
         result (jdbc/query db-spec
                            ["SELECT * FROM users WHERE username = ? AND password = ?"
                             username password])]
     (if (empty? result)
       nil
       (first result))))
+
+(defn add-user [host dbname username password account-type]
+  (let [db-spec (create-db-spec host dbname)]
+    (jdbc/insert! db-spec :users {:username username
+                                  :password password
+                                  :account_type account-type})))
+
+(defn delete-user [host dbname username]
+  (let [db-spec (create-db-spec host dbname)]
+    (jdbc/delete! db-spec :users ["username = ?" username])))
